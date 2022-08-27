6c1b188aa02a6e01032cb6f5aa802845cdb82e45
bozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-27 16:59:52 +0200

    pytest coverage tests

--- cards/tests.py
+++ cards/tests.py
@@ -1,3 +1,16 @@
+import pytest
 from django.test import TestCase
+from cards.models import Card
 
-# Create your tests here.
+@pytest.mark.django_db
+class TestModelCard:
+
+    def test_move_method(self):
+        c = Card.objects.create(question='house', answer='dom', box=1)
+        assert c.box == 1
+        c.move(solved=True)
+        assert c.box == 2
+
+    def test_str(self):
+        c = Card.objects.create(question='house', answer='dom', box=1)
+        assert str(c) == 'house'
